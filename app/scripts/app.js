d1409f5f981243032e1a94e41ca9f480e81c5fe6
Travis Russi <travis@russigroup.com>
2014-07-26 12:04:47 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+angular.module('Barmix', ['ionic', 'Barmix.controllers'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('intro', {
+    url: '/',
+    templateUrl: 'templates/intro.html',
+    controller: 'IntroCtrl'
+  })
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main.html',
+    controller: 'MainCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/');
+
+});
