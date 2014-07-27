adde0c81b3ec2add6fb29931d4c9b6e221307895
Travis Russi <travis@russigroup.com>
2014-07-27 01:16:26 -0400

    asdf

--- parseTwilio/cloud/main.js
+++ parseTwilio/cloud/main.js
@@ -1,19 +1,28 @@
 
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+
+
+
 
 // Require and initialize the Twilio module with your credentials
-var client = require('twilio')('ACCOUNT_SID', 'AUTH_TOKEN');
+//var client = require('twilio')('AC72a1fbdca67d3541bc8aada6508c3bf8', '53a877386beaa14ca994e2d49d500ccc');
 
 // Send an SMS message
-client.sendSms({
-        to:'+19168330413',
-        from: '+19167643946',
-        body: 'Hello world!'
-    }, function(err, responseData) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(responseData.from);
-            console.log(responseData.body);
-        }
-    }
-);
+//client.sendSms({
+//        to:'+19168330413',
+//        from: '+19167643946',
+//        body: 'Hello world!'
+//    }, function(err, responseData) {
+//        if (err) {
+//            console.log(err);
+//        } else {
+//            console.log(responseData.from);
+//            console.log(responseData.body);
+//        }
+//    }
+//);
